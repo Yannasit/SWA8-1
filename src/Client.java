c2cc5e5f01d8f96ecbeff1960e780457717c98fb
Yannasit <624259011@webmail.npru.ac.th>
2021-08-23 15:00:23 +0700

    john

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,8 @@
 
 public class Client {
 	public static void main(String[] args) {
-		Person join = new Person("Mr.", "John", 25);
-		Employee emp = new Employee(join, 50000);
+		Person john = new Person("Mr.", "John", 25);
+		Employee emp = new Employee(john, 50000);
 		emp.showDetail();
 	}
 }
